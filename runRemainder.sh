e317bd0354a0986e1467377227e5323f12ecff67
Rui Pereira <ruipereira@di.uminho.pt>
2018-08-01 14:37:07 +0100

    rest

--- runRemainder.sh
+++ runRemainder.sh
@@ -1,3 +1,21 @@
+cd Hailstone-sequence
+python compile_all.py make measure
+cd ..
 cd Caesar-cipher
+python compile_all.py make measure
+cd ..
+cd Sorting-algorithms-Quicksort
+python compile_all.py make measure
+cd ..
+cd Sieve-of-Eratosthenes
+python compile_all.py make measure
+cd ..
+cd ABC-Problem
+python compile_all.py make measure
+cd ..
+cd N-queens-problem
+python compile_all.py make measure
+cd ..
+cd 100-doors
 python compile_all.py make measure
 cd ..
